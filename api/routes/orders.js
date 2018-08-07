255a352eda532835539ebaead096ca19fe7dc343
SmallRed <ssh137452596@gmail.com>
2018-08-07 16:00:58 +0800

    populated queries with mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,29 @@ const Order = require('./../models/order')
 
 router.get("/", (req, res, next) => {
     Order.find()
+        .populate('product', 'name price')
         .select("product quantity _id")
         .then(docs => {
             res.status(200).json(docs);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.get("/:orderId", (req, res, next) => {
+    Order.findById(req.params.orderId)
+        .populate('product', 'name price')
+        .select("product quantity _id")
+        .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "Order not found"
+                });
+            }
+            res.status(200).json(order);
         })
         .catch(err => {
             res.status(500).json({
@@ -32,24 +52,6 @@ router.post("/", (req, res, next) => {
         })
         .then(result => {
             res.status(201).json(result);
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
-});
-
-router.get("/:orderId", (req, res, next) => {
-    Order.findById(req.params.orderId)
-        .select("product quantity _id")
-        .then(order => {
-            if (!order) {
-                return res.status(404).json({
-                    message: "Order not found"
-                });
-            }
-            res.status(200).json(order);
         })
         .catch(err => {
             res.status(500).json({
